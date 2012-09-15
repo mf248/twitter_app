7d05e8205f3ec8beb3009bc489b32e927d529bd8
Michael Fergus <mfergus48@gmail.com>
2012-09-15 17:33:27 -0500

    Revert to de9a42f7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 TwitterApp::Application.routes.draw do
-
   get "users/new"
 
   root to: 'static_pages#home'
-
-  match '/' to: 'static_pages#home'
 
   match '/signup', to: 'users#new'
 
@@ -15,7 +12,6 @@ TwitterApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
 
   match '/contact', to: 'static_pages#contact'
-
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
